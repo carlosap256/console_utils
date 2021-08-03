f5fa446f7a82ab7316aa63b275ee4e4b83350c58
Carlos Hernan Arce Plata <carlos.arce-plata@open.ac.uk>
2021-08-03 13:15:14 +0000

    Improved init kubernetes script to avoid redoing steps

--- kubernetes/init_kubernetes.sh
+++ kubernetes/init_kubernetes.sh
@@ -1,42 +1,63 @@
 #!/bin/bash
 
-echo "Installing Kubernetes from snap"
 
-sudo snap install microk8s --classic
+is_kube_installed=$(snap info microk8s | grep "installed")
+if [ -z "$is_kube_installed" ];then
+    echo "Installing Kubernetes from snap"
+    sudo snap install microk8s --classic
+else
+    kube_version=$(echo "${is_kube_installed}" | awk '{print $2}')
+    echo "Kubernetes already installed"
+    echo "Version ${kube_version}"
+fi
 
-#echo "Adding current user to microk8s group"
-#username=$(whoami)
-#sudo usermod -a -G microk8s "${username}"
+username=$(whoami)
+user_in_kubernetes_group=$(groups ${username} | grep "microk8s")
+
+if [ -z "$user_in_kubernetes_group" ];then
+    echo "Adding current user to microk8s group"
+    sudo usermod -a -G microk8s "${username}"
+    echo "Need log out and in again and rerun this script for the changes to take effect"
+else
+    echo "Current user is already in Kubernetes group"
+fi
 
 echo "Adding dns dashboard and storage services"
 microk8s.enable dns dashboard storage
 
-echo "Creating a service account"
-microk8s.kubectl create -n kube-system serviceaccount admin
-microk8s.kubectl -n kube-system get serviceaccount admin -o yaml
 
+admin_service_account=$(microk8s.kubectl -n kube-system get serviceaccount admin -o yaml)
+if [ -z "$admin_service_account" ];then
 
-echo "Extracting the token from the created account"
+    echo "Creating a service account"
+    microk8s.kubectl create -n kube-system serviceaccount admin
+else
+    echo "Admin account already created"
+fi
+
+echo "Extracting the token from the admin account"
 secret_name=$(microk8s.kubectl -n kube-system get serviceaccount admin -o jsonpath='{.secrets[0].name}')
 token=$(microk8s.kubectl -n kube-system get secret "${secret_name}" -o jsonpath='{.data.token}' | base64 --decode)
 
 
-
-echo "EXPOSE THE DASHBOARD"
-echo "Change .spec.type to NodePort"
-#microk8s.kubectl -n kube-system edit service kubernetes-dashboard
-#kubernetes-dashboard   NodePort   10.152.183.70   <none>        443:31975/TCP   3h20m
-microk8s.kubectl -n kube-system patch service kubernetes-dashboard -p '{"spec":{"type":"NodePort"}}'
-
-
+dashboard_spec_type=$(microk8s.kubectl -n kube-system get service kubernetes-dashboard -o jsonpath='{.spec.type}')
+if [ "$dashboard_spec_type" = "NodePort" ]; then
+    echo "Dashboard already exposed"
+else
+    echo "EXPOSE THE DASHBOARD"
+    echo "Changing .spec.type to NodePort"
+    #microk8s.kubectl -n kube-system edit service kubernetes-dashboard
+    #kubernetes-dashboard   NodePort   10.152.183.70   <none>        443:31975/TCP   3h20m
+    microk8s.kubectl -n kube-system patch service kubernetes-dashboard -p '{"spec":{"type":"NodePort"}}'
+fi
 
 dashboard_ip=$(microk8s.kubectl get service/kubernetes-dashboard --namespace kube-system -o jsonpath='{.spec.clusterIP}')
 dashboard_port=$(microk8s.kubectl -n kube-system get service kubernetes-dashboard -o jsonpath='{.spec.ports[*].nodePort}')
 
 
-echo "Now the dashboard is visible in https://localhost:${dashboard_port}"
+echo -e "\n\nThe dashboard is visible in https://localhost:${dashboard_port}"
 echo "https://${dashboard_ip}:${dashboard_port}"
 
-echo -e "The token to access the dashbaord is:\n"
+echo -e "The token to access the dashboard is:\n"
 echo "${token}"
 echo -e "\n"
