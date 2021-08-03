e223325e0a27648a44a747e7708fa0dd4863de6f
Carlos Hernan Arce Plata <carlos.arce-plata@open.ac.uk>
2021-08-03 13:59:34 +0000

    Small tweaks

--- kubernetes/apply_dashboard_certs.sh
+++ kubernetes/apply_dashboard_certs.sh
@@ -3,14 +3,20 @@
 # This assumes the dashboard is in 'kube-system' namespace, but 
 # the new version seem to be in its own namespace 'kubernetes-dashboard'
 
-echo "Deleting original empty secret"
-microk8s.kubectl delete secret kubernetes-dashboard-certs -n kube-system
+dashboard_certs=$(microk8s.kubectl -n kube-system get secret kubernetes-dashboard-certs | grep "kubernetes-dashboard-certs" | awk '{print $3}')
+if [ "$dashboard_certs" = "0" ];then
+    echo "Deleting original empty secret"
+    microk8s.kubectl delete secret kubernetes-dashboard-certs -n kube-system
 
-echo "Creating secret with dashboard keys"
-microk8s.kubectl create secret generic kubernetes-dashboard-certs --from-file="$HOME/kubernetes_cert/dashboard.key" --from-file="$HOME/kubernetes_cert/dashboard.crt" -n kube-system
+    echo "Creating secret with dashboard keys"
+    microk8s.kubectl create secret generic kubernetes-dashboard-certs --from-file="$HOME/kubernetes_cert/dashboard.key" --from-file="$HOME/kubernetes_cert/dashboard.crt" -n kube-system
+    
+    echo "Delete dashboard pods to restart them with the certs"
+    microk8s.kubectl get pod -n kube-system | grep "dashboard" |  awk '{print "kubectl delete po " $1 " -n kube-system"}' | sh
 
-echo "Delete dashboard pods to restart them with the certs"
-microk8s.kubectl get pod -n kube-system | grep "dashboard" |  awk '{print "kubectl delete po " $1 " -n kube-system"}' | sh
+else
+    echo "Dashboard already has certs installed"
+fi
 
 
 
--- kubernetes/init_kubernetes.sh
+++ kubernetes/init_kubernetes.sh
@@ -9,6 +9,7 @@ else
     kube_version=$(echo "${is_kube_installed}" | awk '{print $2}')
     echo "Kubernetes already installed"
     echo "Version ${kube_version}"
+    echo ""
 fi
 
 username=$(whoami)
@@ -20,6 +21,7 @@ if [ -z "$user_in_kubernetes_group" ];then
     echo "Need log out and in again and rerun this script for the changes to take effect"
 else
     echo "Current user is already in Kubernetes group"
+    echo ""
 fi
 
 echo "Adding dns dashboard and storage services"
@@ -33,6 +35,7 @@ if [ -z "$admin_service_account" ];then
     microk8s.kubectl create -n kube-system serviceaccount admin
 else
     echo "Admin account already created"
+    echo ""
 fi
 
 echo "Extracting the token from the admin account"
@@ -43,6 +46,7 @@ token=$(microk8s.kubectl -n kube-system get secret "${secret_name}" -o jsonpath=
 dashboard_spec_type=$(microk8s.kubectl -n kube-system get service kubernetes-dashboard -o jsonpath='{.spec.type}')
 if [ "$dashboard_spec_type" = "NodePort" ]; then
     echo "Dashboard already exposed"
+    echo ""
 else
     echo "EXPOSE THE DASHBOARD"
     echo "Changing .spec.type to NodePort"
@@ -55,9 +59,10 @@ dashboard_ip=$(microk8s.kubectl get service/kubernetes-dashboard --namespace kub
 dashboard_port=$(microk8s.kubectl -n kube-system get service kubernetes-dashboard -o jsonpath='{.spec.ports[*].nodePort}')
 
 
-echo -e "\n\nThe dashboard is visible in https://localhost:${dashboard_port}"
-echo "https://${dashboard_ip}:${dashboard_port}"
+echo -e "\n\nThe dashboard is visible in"
+echo -e "https://localhost:${dashboard_port}"
+echo -e "https://${dashboard_ip}:${dashboard_port}"
 
-echo -e "The token to access the dashboard is:\n"
+echo -e "\n\nThe token to access the dashboard is:\n"
 echo "${token}"
 echo -e "\n"
