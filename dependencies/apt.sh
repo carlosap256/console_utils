b2dac7ead65a968af12f462706f4bcce394be1b8
Carlos Hernan Arce Plata <carlos.arce-plata@open.ac.uk>
2021-08-09 13:52:32 +0000

    Removed deprecated dependencies

--- dependencies/apt.sh
+++ dependencies/apt.sh
@@ -13,52 +13,52 @@ sudo snap install yq
 
 
 
-echo -e "Install Vim syntax checking with (1) Pathogen? or with (2) Spacevim? "
-read -r -n1 option
+#echo -e "Install Vim syntax checking with (1) Pathogen? or with (2) Spacevim? "
+#read -r -n1 option
 
-if [ "$option" == "1" ]
-then
+#if [ "$option" == "1" ]
+#then
 
-    # Install Pathogen
-    if [ ! -f ~/.vim/autoload/pathogen.vim ]
-    then
-        mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-    fi
-
-    # Bash syntax checking 
-    sudo apt-get install -y vim vim-syntastic shellcheck
-
-    # Install syntastic as a Pathogen bundle
-    cd ~/.vim/bundle && \
-    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
-
-elif [ "$option" == "2" ]
-then
+#    # Install Pathogen
+#    if [ ! -f ~/.vim/autoload/pathogen.vim ]
+#    then
+#        mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+#        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#    fi
+#
+#    # Bash syntax checking 
+#    sudo apt-get install -y vim vim-syntastic shellcheck
+#
+#    # Install syntastic as a Pathogen bundle
+#    cd ~/.vim/bundle && \
+#    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+#
+#elif [ "$option" == "2" ]
+#then
     original_user=$(logname)
     # Install spacevim
     su "$original_user" -c "curl -sLf https://spacevim.org/install.sh | bash"
-else
-    echo "Invalid option '$option'.  Skipping..."
-fi    
+#else
+#    echo "Invalid option '$option'.  Skipping..."
+#fi    
 
 
-echo -e "Install (1) Markdown? or (2) Retext? "
-read -r -n1 option
-
-if [ "$option" == "1" ]
-then
-    # Install Markdown for Git
-    cd /tmp &&
-    wget https://remarkableapp.github.io/files/remarkable_1.62_all.deb &&
-    sudo apt-get install gdebi-core &&
-    sudo gdebi remarkable_1.62_all.deb
-elif [ "$option" == "2" ]
-then
+#echo -e "Install (1) Markdown? or (2) Retext? "
+#read -r -n1 option
+#
+#if [ "$option" == "1" ]
+#then
+#    # Install Markdown for Git
+#    cd /tmp &&
+#    wget https://remarkableapp.github.io/files/remarkable_1.62_all.deb &&
+#    sudo apt-get install gdebi-core &&
+#    sudo gdebi remarkable_1.62_all.deb
+#elif [ "$option" == "2" ]
+#then
     # Install fallback Markdown tool
     sudo apt-get install retext
-else
-    echo "Invalid option '$option'.  Skipping..."
-fi
+#else
+#    echo "Invalid option '$option'.  Skipping..."
+#fi
 
 
