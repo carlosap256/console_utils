0362a6c49c9455c1e167725ddba961a6a13cd38b
Carlos Hernan Arce Plata <carlos.arce-plata@open.ac.uk>
2021-10-19 13:40:01 +0000

    Krew installer

--- kubernetes/init_kubernetes.sh
+++ kubernetes/init_kubernetes.sh
@@ -74,3 +74,15 @@ echo -e "https://${dashboard_ip}:${dashboard_port}"
 echo -e "\n\nThe token to access the dashboard is:\n"
 kube_token.sh
 echo -e "\n"
+
+echo "Installing Krew"
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
