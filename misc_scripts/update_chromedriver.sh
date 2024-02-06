a0206255373b88ea2fdeca90f6cf1edb36115c02
Carlos Hernan Arce Plata <carlos.arce-plata@open.ac.uk>
2024-02-06 12:24:10 +0000

    Added script to update selenium driver for linux

---
+++ misc_scripts/update_chromedriver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Legacy update file version
+#############################
+#release_major_number=$1
+#latest_version=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${release_major_number})
+
+#latest_version=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+#echo "Latest chrome version: $latest_version"
+#wget -c https://chromedriver.storage.googleapis.com/${latest_version}/chromedriver_linux64.zip
+
+
+
+
+
+installer_file_url=`curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json | yq e '.channels.Stable.downloads.chromedriver.[] | select(.platform == "linux64") | .url'`
+wget ${installer_file_url}
+
+
+
+unzip chromedriver-linux64.zip 
+rm chromedriver-linux64.zip 
+sudo cp -f chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
+rm -r chromedriver-linux64/
